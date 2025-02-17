49ef0010e16f49fab0d99bfbcac209d674ed0a68
JORGE SANTANA PRADO <154940965+xorge1998@users.noreply.github.com>
2025-02-17 15:37:05 +0000

    make `database.js` more robust to error handling and finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from 'infra/database.js';
+import { version } from 'react';
 
 async function status (req, res) {
-  const result = await database.query('SELECT 1+1 as sum;');
-  console.log(result.rows);
-  res.status(200).json({ status: 'ok' });
-};
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query('SHOW server_version;');
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query('SHOW max_connections;');
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [databaseName],
+  });
+    const databaseOpenConnectionsValue = databaseOpenConnectionsResult.rows[0].count;
+
+
+  res.status(200).json({ 
+  updated_at : updatedAt,
+  dependencies: {
+    database: {
+      version: databaseVersionValue,
+      maxConnections: parseInt(databaseMaxConnectionsValue),
+      opened_connections: databaseOpenConnectionsValue,
+    },
+  },
+});
+}
 
 export default status;
