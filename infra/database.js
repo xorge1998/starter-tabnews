b062a4eeb4b9167387f9ef8bc6c1d024e2f3bcbb
JORGE SANTANA PRADO <154940965+xorge1998@users.noreply.github.com>
2025-02-20 18:32:42 +0000

    handles `database ssl connection` and parammeters in `.env.developmento`

--- infra/database.js
+++ infra/database.js
@@ -7,8 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
-    sslmode: process.env.NODE_ENV === 'development' ? 'disable' : 'require',
+    ssl: process.env.NODE_ENV === 'production'? true : false,
   });
 
   try {
@@ -25,4 +24,4 @@ async function query(queryObject) {
 
 export default {
   query: query,
-}; 
+};
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,7 +7,8 @@ test("GET to /api/v1/status should returns 200", async () => {
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString(); 
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("16.8");
+  //expect(["16.0","16.1", "16.4","16.8"]).toContain(responseBody.dependencies.database.version);
   expect(responseBody.dependencies.database.maxConnections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 
