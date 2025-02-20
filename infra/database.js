aad0d4f2f7a4431918a13a918625d50c736635b3
JORGE SANTANA PRADO <154940965+xorge1998@users.noreply.github.com>
2025-02-20 15:12:38 +0000

    add `ssl required mode`for more robust security

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === 'development' ? false : true,
+    sslmode: process.env.NODE_ENV === 'development' ? 'disable' : 'require',
   });
 
   try {
