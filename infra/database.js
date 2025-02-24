7766b9f6c5409a1510403bba777845213287b35d
JORGE SANTANA PRADO <154940965+xorge1998@users.noreply.github.com>
2025-02-24 14:39:29 +0000

    adjust for error `sslmode = required` and att postgres verions `16.8`

--- infra/database.js
+++ infra/database.js
@@ -7,8 +7,16 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'production'? true : false,
+    ssl: true,//process.env.NODE_ENV === "development"? false : true,
   });
+
+console.log('itens do client : ', { host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  ssl: process.env.NODE_ENV === "development"?false:true,
+});
 
   try {
     await client.connect();
