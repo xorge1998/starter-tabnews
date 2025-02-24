e6af95425fc6eb6e2554b2eed3634d357d338c28
JORGE SANTANA PRADO <154940965+xorge1998@users.noreply.github.com>
2025-02-24 14:49:20 +0000

    removing test `console.log`

--- infra/database.js
+++ infra/database.js
@@ -10,14 +10,6 @@ async function query(queryObject) {
     ssl: true,//process.env.NODE_ENV === "development"? false : true,
   });
 
-console.log('itens do client : ', { host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl: process.env.NODE_ENV === "development"?false:true,
-});
-
   try {
     await client.connect();
     const result = await client.query(queryObject);
